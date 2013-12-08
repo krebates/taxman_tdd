f19c63eaa9fcb5b87f95debb707626fb4d18e0b4
Krystle Bates <krebates@gmail.com>
2013-12-07 22:04:21 -0500

    added liability rb and spec file

--- spec/taxman_tdd/liability_spec.rb
+++ spec/taxman_tdd/liability_spec.rb
@@ -4,3 +4,21 @@
 #The amount_due represents taxes that must be paid by the employee.
 #The amount_owed represents the refund.
 
+require 'rspec'
+require 'pry'
+
+require_relative '../../lib/liability.rb'
+
+describe Liability do
+  let(:data) {{"annual_income" => 120000.0, "tax_paid" => 28000, "tax_rate" => 38.0}}
+  let(:x) { Liability.new(data) }
+
+  it 'represents taxes that must be paid by the employee' do
+    expect(x.amount_due).to eql(45600.0)
+  end
+
+  it 'represents the refund due' do
+    expect(x.amount_owed).to eql(-17600.0)
+  end
+end
+
